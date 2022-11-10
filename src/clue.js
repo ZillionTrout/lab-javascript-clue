c21fa835ae4a3c2060781925d2a86db88cd1d78b
Estefania Egea <estefania.egcal@gmail.com>
2022-11-10 11:58:54 +0100

    añadidos algunos cambios. Siguen sin funcionar :_)

--- src/clue.js
+++ src/clue.js
@@ -1,115 +1,114 @@
-// ITERATION 1
+// // ITERATION 1
 
-// Suspects Array
+// // Suspects Array
 
-const suspectsArray = [
-    {
-        firstName: 'Jacob',
-        lastName: 'Green',
-        occupation: 'Entrepreneur',
-        age: 45,
-        description: 'He has a lot of connections',
-        image: 'http://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-        color: 'green',
-    },
-    {
-        firstName: 'Doctor',
-        lastName: 'Orchid',
-        occupation: 'Scientist',
-        age: 26,
-        description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
-        image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-        color: 'white',
+const mrGreen = {
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur',
+    age: 45,
+    description: 'He has a lot of connections',
+    image: 'http://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    color: 'green',
+};
+const drOrchid = {
+    firstName: 'Doctor',
+    lastName: 'Orchid',
+    occupation: 'Scientist',
+    age: 26,
+    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'white',
 
-    },
-    {
-        firstName: 'Victor',
-        lastName: 'Plum',
-        occupation: 'design',
-        age: 22,
-        description: 'Billionaire video game designer',
-        image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
-        color: 'purple',
-    },
-    {
-        firstName: 'Kasandra',
-        lastName: 'Scarlet',
-        occupation: 'Actor',
-        age: 31,
-        description: 'She is an A-list movie star with a dark past',
-        image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
-        color: 'red',
+};
+const profPlum = {
+    firstName: 'Victor',
+    lastName: 'Plum',
+    occupation: 'design',
+    age: 22,
+    description: 'Billionaire video game designer',
+    image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+    color: 'purple',
+};
+const missScarlet = {
+    firstName: 'Kasandra',
+    lastName: 'Scarlet',
+    occupation: 'Actor',
+    age: 31,
+    description: 'She is an A-list movie star with a dark past',
+    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'red',
 
-    },
-    {
-        firstName: 'Elanor',
-        lastName: 'Peacock',
-        occupation: 'Socialité',
-        age: 36,
-        description: 'She is from a wealthy family and uses her status and money to earn popularity',
-        image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-        color: 'blue',
-    },
-    {
-        firstName: 'Jack',
-        lastName: 'Mustard',
-        occupation: 'Retired football player',
-        age: 62,
-        description: 'He is a former football player who tries to get by on his former glory',
-        image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
-        color: 'yellow',
-    }
-];
+};
+const mrsPeacock = {
+    firstName: 'Elanor',
+    lastName: 'Peacock',
+    occupation: 'Socialité',
+    age: 36,
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    color: 'blue',
+};
+const mrMustard = {
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired football player',
+    age: 62,
+    description: 'He is a former football player who tries to get by on his former glory',
+    image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    color: 'yellow',
+};
+
+const suspectsArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard]
 
 // Rooms Array
 
-const roomsArray = [
-    {name: 'Dining Room'},
-    {name: 'Conservatory'},
-    {name: 'Kitchen'},
-    {name: 'Study'},
-    {name: 'Library'},
-    {name: 'Billiard Room'},
-    {name: 'Lounge'},
-    {name: 'Ballroom'},
-    {name: 'Hall'},
-    {name: 'Spa'},
-    {name: 'Living Room'},
-    {name: 'Observatory'},
-    {name: 'Theater'},
-    {name: 'Guest House'},
-    {name: 'Patio'},
-];
+const diningRoom = {name: 'Dining Room'};
+const conservatory ={name: 'Conservatory'};
+const kitchen ={name: 'Kitchen'};
+const Study ={name: 'Study'};
+const library ={name: 'Library'};
+const billiardRoom = {name: 'Billiard Room'};
+const lounge = {name: 'Lounge'};
+const ballRoom = {name: 'Ballroom'};
+const hall = {name: 'Hall'};
+const spa = {name: 'Spa'};
+const livingRoom = {name: 'Living Room'};
+const observatory = {name: 'Observatory'};
+const theater = {name: 'Theater'};
+const guestHouse = {name: 'Guest House'};
+const patio = {name: 'Patio'};
+
+const roomsArray = [diningRoom, conservatory, kitchen, Study, library, billiardRoom, lounge, ballRoom, hall, spa, livingRoom, observatory, theater, guestHouse, patio];
 
 // Weapons Array
 
-const weaponsArray = [
-    {name: 'rope', weight: 10},
-{name: 'knife', weight: 8},
-{name: 'candlestick', weight: 2},
-{name: 'dumbbell', weight: 30},
-{name: 'poison', weight: 2},
-{name: 'axe', weight: 15},
-{name: 'bat', weight: 13},
-{name: 'trophy', weight: 25},
-{name: 'pistol', weight: 20},
-];
+const rope = {name: 'rope', weight: 10};
+const knife = {name: 'knife', weight: 8};
+const candlestick = {name: 'candlestick', weight: 2};
+const dumbbell = {name: 'dumbbell', weight: 30};
+const poison = {name: 'poison', weight: 2};
+const axe = {name: 'axe', weight: 15};
+const bat = {name: 'bat', weight: 13};
+const trophy = {name: 'trophy', weight: 25};
+const pistol = {name: 'pistol', weight: 20};
+
+const weaponsArray = [rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol];
 
 
-// ITERATION 2
+// // ITERATION 2
 
-function selectRandom(myArray) {
-    if(myArray.lenght === 0) return undefined;
-    if(myArray.lenght ===1) return myArray[0];
-    const randomElement = myArray[(Math.random() *myArray.lenght) | 0];
-    return randomElement;
+function selectRandom(myArray) {    
+     const randomElement = myArray[(Math.random() *myArray.lenght) | 0 ];
+    return randomElement; 
     }
+//Falta "should return a random element of the array"
 
 function pickMystery() {
     const mystery = {
-        suspect : selectRandom(suspectsArray),
-        weapon : selectRandom(weaponsArray),
-        room : selectRandom(roomsArray),
+    suspect : selectRandom(suspectsArray),
+    weapon : selectRandom(weaponsArray),
+    room : selectRandom(roomsArray),
     };
     return mystery;
 };
@@ -117,5 +116,6 @@ function pickMystery() {
 // ITERATION 3
 
 function revealMystery(randomObj) {
-return `${randomObj.suspect.firstName} ${randomObj.suspect.lastName} killed MediaRecorder. Boddy using the ${randomObj.weapon.name} in the ${randomObj.room.name}!`;
-}
+    return `${randomObj.suspect.firstName} ${randomObj.suspect.LastName} killed Mr Boddy using the ${randomObj.weapon.name} in the ${randomObj.room.name}!`
+};
+//falta "should return "<FIRST NAME> <LAST NAME> killed Mr. Boddy using the <WEAPON> in the <PLACE>!""
